72b23a58d8f3a6a4a450ff6d46529e6746f1f9ae
Malcolm-Tompkins <82959788+Malcolm-Tompkins@users.noreply.github.com>
2021-12-02 13:16:29 -0500

    Update package-info.java

--- package-info.java
+++ package-info.java
@@ -1,7 +1,8 @@
-s is the package-info.java file.
+/**
+* This is the package-info.java file.
 *
 * @author  Malcolm Tompkins
 * @version 1.0
-* @since   2020-12-01
+* @since   2020-11-24
 */
 package ca.mths.ics4u;
